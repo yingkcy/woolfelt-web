f22df9d99dbc2b53f5851820c6c6550bd6f8191f
Cheng Ying <yingkcya9623@gmail.com>
2026-03-29 01:53:45 +0800

    update

--- JS/detail.js
+++ JS/detail.js
@@ -7,10 +7,15 @@ document.getElementById("main-img").src = work.img;
 document.getElementById("ref-img").src = work.ref;
 
 document.getElementById("author").innerText =
-  work.name + "（" + work.seat + "）";
+  work.name + "  " + work.seat + "  ";
 
 document.getElementById("title_zh").innerText = work.title_zh;
 document.getElementById("title_en").innerText = work.title_en;
 
 document.getElementById("desc_zh").innerText = work.desc_zh;
 document.getElementById("desc_en").innerText = work.desc_en;
+
+document.querySelector(".back-btn").addEventListener("click", () => {
+  history.back();
+});
+
